75e8dadb177d68da497861fc09bc7ec3751f5e1f
garasubo <garasubo@gmail.com>
2023-10-08 15:55:28 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -794,7 +794,7 @@ fn main() -> Result<(), anyhow::Error> {
         Command::RunBench(args) => run_bench(args),
     };
     if let Err(err) = result {
-        println!("{:?}", err.backtrace());
+        println!("{:?}", err.root_cause());
         return Err(err);
     }
     Ok(())
